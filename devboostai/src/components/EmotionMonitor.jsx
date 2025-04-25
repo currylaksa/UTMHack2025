c0b62b179c1af3101b18a09b9a27d5437105ed41
currylaksa <qingyee0219@gmail.com>
2025-04-25 20:19:51 +0800

    update managerdashboard

---
+++ devboostai/src/components/EmotionMonitor.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useEmotion } from '../services/EmotionContext';
+import { 
+  FaceSmileIcon, 
+  FaceFrownIcon, 
+  QuestionMarkCircleIcon,
+  FireIcon,
+  ClockIcon,
+  HeartIcon,
+  ExclamationCircleIcon
+} from '@heroicons/react/24/outline';
